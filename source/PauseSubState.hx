25a61812d86dfce01b30bd7f671bd35b92249406
User-9103 <120707466+User-9103@users.noreply.github.com>
2023-02-15 18:40:40 +0700

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -60,7 +60,6 @@ class PauseSubState extends MusicBeatSubstate
 		}
 		difficultyChoices.push('BACK');
 
-
 		pauseMusic = new FlxSound();
 		if(songName != null) {
 			pauseMusic.loadEmbedded(Paths.music(songName), true, true);
@@ -134,10 +133,17 @@ class PauseSubState extends MusicBeatSubstate
 		regenMenu();
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 
-                #if android
-                addVirtualPad(UP_DOWN, A);
-                addPadCamera();
-                #end
+		#if android
+		if (PlayState.chartingMode)
+		{
+			addVirtualPad(LEFT_FULL, A);
+		}
+		else
+		{
+			addVirtualPad(UP_DOWN, A);
+		}
+		addPadCamera();
+		#end
 	}
 
 	var holdTime:Float = 0;
